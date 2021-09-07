dafabf3a89c8a304933e66d0a29afb4c08352dde
mAhangari <morteza_ahangari_1994@yahoo.com>
2021-09-08 03:22:16 +0430

    Publish TweetService v0.1

---
+++ Twitter/src/main/java/ir/maktab56/Twitter/service/TweetService.java
@@ -0,0 +1,8 @@
+package ir.maktab56.Twitter.service;
+
+import ir.maktab56.Twitter.domain.Tweet;
+import ir.maktab56.Twitter.base.service.BaseEntityService;
+
+public interface TweetService extends BaseEntityService<Tweet, Long> {
+
+}
