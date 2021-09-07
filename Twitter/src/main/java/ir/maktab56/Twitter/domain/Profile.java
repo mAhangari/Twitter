4659c93d2140b2de24a90e07271ec0019f08bd0b
mAhangari <morteza_ahangari_1994@yahoo.com>
2021-09-08 03:17:52 +0430

    Publish Profile v0.1

--- Twitter/src/main/java/ir/maktab56/Twitter/domain/Profile.java
+++ Twitter/src/main/java/ir/maktab56/Twitter/domain/Profile.java
@@ -2,16 +2,23 @@ package ir.maktab56.Twitter.domain;
 
 import java.util.HashSet;
 import java.util.Set;
-
 import javax.persistence.*;
 import ir.maktab56.Twitter.base.domain.BaseEntity;
 
 @Entity
 @Table(name = Profile.TABLE_NAME)
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+//@NamedEntityGraph(name = Profile.FETCH_EMAIL_AND_PHONENUMBER,
+//		attributeNodes = {
+//				@NamedAttributeNode(value = "email"),
+//				@NamedAttributeNode(value = "phoneNumbers")
+//		}
+//	)
+
 public class Profile extends BaseEntity<Long> {
 
     public static final String TABLE_NAME = "profile_table";
+    public static final String FETCH_EMAIL_AND_PHONENUMBER = "FETCH_EMAIL_AND_PHONENUMBER";
 
     private String firstName;
 
@@ -22,23 +29,16 @@ public class Profile extends BaseEntity<Long> {
 
     private String password;
     
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "profile_email", joinColumns = @JoinColumn(name = "profile_id"))
     private Set<String> email;
     
-    @ElementCollection
-    private Set<String> mobileNumber = new HashSet<>();
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> phoneNumbers = new HashSet<>();
 
     private Boolean isActive;
 
     public Profile() {
-    }
-
-    @Override
-    public String toString() {
-        return "Profile{" +
-                "id='" + getId() + '\'' +
-                "firstName='" + firstName + '\'' +
-                '}';
     }
 
     public String getFirstName() {
@@ -80,13 +80,21 @@ public class Profile extends BaseEntity<Long> {
     public void setEmail(Set<String> email) {
         this.email = email;
     }
-
-    public Set<String> getMobileNumber() {
-        return mobileNumber;
+    
+    public void addEmail(String email) {
+    	this.email.add(email);
     }
 
-    public void setMobileNumber(Set<String> mobileNumber) {
-        this.mobileNumber = mobileNumber;
+    public Set<String> getPhoneNumber() {
+        return phoneNumbers;
+    }
+
+    public void setPhoneNumber(Set<String> phoneNumbers) {
+        this.phoneNumbers = phoneNumbers;
+    }
+    
+    public void addPhoneNumber(String phoneNumber) {
+    	this.phoneNumbers.add(phoneNumber);
     }
 
     public Boolean getActive() {
@@ -95,5 +103,18 @@ public class Profile extends BaseEntity<Long> {
 
     public void setActive(Boolean active) {
         isActive = active;
+    }
+    
+    @Override
+    public String toString() {
+        return "Profile:\n" +
+                "id='" + getId() + "\'\t" +
+                "firstName='" + firstName + "\'\t" +
+                "lastName='" + lastName + "\'\n" +
+                "username='" + username + "\'\t" +
+                "password='" + password + "\'\t" +
+                "email='" + email + "\'\n" +
+                "phoneNumbers='" + phoneNumbers + "\'\n" +
+                '}';
     }
 }
