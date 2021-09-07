5b0580bf0ca882a96f5b1b479450a7e9b3b34781
mAhangari <morteza_ahangari_1994@yahoo.com>
2021-09-08 03:18:29 +0430

    Publish Customer v0.1

--- Twitter/src/main/java/ir/maktab56/Twitter/domain/Customer.java
+++ Twitter/src/main/java/ir/maktab56/Twitter/domain/Customer.java
@@ -3,23 +3,32 @@ package ir.maktab56.Twitter.domain;
 import java.sql.Date;
 import java.util.HashSet;
 import java.util.Set;
-
 import javax.persistence.*;
 
 @Entity
+@NamedEntityGraph(name = Customer.FETCH_TWEET_AND_COMMENT,
+			attributeNodes = {
+					@NamedAttributeNode(value = "tweets", subgraph = "commentOfTweet"),
+			},
+			subgraphs = {
+					@NamedSubgraph(name = "commentOfTweet",
+							attributeNodes = {
+									@NamedAttributeNode(value = "comments")
+							}
+						)
+			}
+			
+		)
 public class Customer extends Profile {
-
+	
+	public static final String FETCH_TWEET_AND_COMMENT = "FETCH_TWEET_AND_COMMENT";
 	@Column(name = "birthday")
 	private Date birthday;
 	
-	@OneToMany(mappedBy = "customer")
-	private Set<Comment> comments = new HashSet<>();
-	
-	@OneToMany(mappedBy = "customer")
-	private Set<Comment> tweets = new HashSet<>();
+	@OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private Set<Tweet> tweets = new HashSet<>();
 	
 	public Customer() {
-		super();
 	}
 	
 	public Customer(String username, String password, String firstName,
@@ -40,22 +49,19 @@ public class Customer extends Profile {
 		this.birthday = birthday;
 	}
 
-	public Set<Comment> getComments() {
-		return comments;
-	}
-
-	public void setComments(Set<Comment> comments) {
-		this.comments = comments;
-	}
-
-	public Set<Comment> getTweets() {
+	public Set<Tweet> getTweets() {
 		return tweets;
 	}
 
-	public void setTweets(Set<Comment> tweets) {
+	public void setTweets(Set<Tweet> tweets) {
 		this.tweets = tweets;
 	}
-
+	
+	public void addTweet(Tweet tweet) {
+		this.tweets.add(tweet);
+	}
+	
+	@Override
 	public String toString() {
 		return  "id='" + getId() + "\' " +
                 "firstName= '" + getFirstName() + "\' " +
@@ -63,7 +69,7 @@ public class Customer extends Profile {
                 "username= '" + getUsername() + "\' " +
                 "password= '" + getPassword() + "\' " +
                 "birthday= '" + birthday + "\' " +
-                //"email= " + getEmail() + " " +
+                "email= " + getEmail() + " " +
                 "phoneNumbers= " + getPhoneNumber();
 	}
 }
